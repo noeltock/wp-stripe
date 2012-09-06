adedd55bf8a9ecea87f2f30d0b4903b893970317
Matt Gibbs <mgibbs@forumone.com>
2012-09-06 14:26:22 -0400

    Meh, hard-coded months are fine

--- includes/stripe-display.php
+++ includes/stripe-display.php
@@ -63,12 +63,18 @@ function wp_stripe_form() {
             <div class="stripe-row-right">
                 <span class="stripe-expiry">EXPIRY</span>
                 <select class="card-expiry-month">
-                <?php
-                    for ( $i = 1; $i <= 12; $i++ ) {
-                        $month = ( $i < 10 ) ? "0$i" : $i;
-                        echo '<option value="' . $month . '">' . $month . '</option>';
-                    }
-                ?>
+                    <option value="1">01</option>
+                    <option value="2">02</option>
+                    <option value="3">03</option>
+                    <option value="4">04</option>
+                    <option value="5">05</option>
+                    <option value="6">06</option>
+                    <option value="7">07</option>
+                    <option value="8">08</option>
+                    <option value="9">09</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
                 </select>
                 <span></span>
                 <select class="card-expiry-year">
